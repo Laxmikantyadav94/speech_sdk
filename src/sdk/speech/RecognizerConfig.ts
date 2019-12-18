ceba85f7595f17195f9cf95cbae7280c9fda3609
Laxmikant Munshilal <laxmikantm@pune.smartek21.st21>
2019-12-18 14:12:54 +0530

    cleanup

--- src/sdk/speech/RecognizerConfig.ts
+++ src/sdk/speech/RecognizerConfig.ts
@@ -1,7 +1,22 @@
 import * as io from "socket.io-client";
 
 export class RecognizerConfig {
-    public ioConnect(): void {
-        io.connect("http://192.168.50.86:3000");
+    socket: any;
+    stream: any;
+
+    public async ioConnect(): Promise<any> {
+       this.socket= io.connect("http://192.168.50.86:3000");
+        this.stream= await this.getMedia();
+    }
+
+    public async getMedia(): Promise<any> {
+        let stream = null;
+        const constraints = { audio: true };
+        try {
+            stream = await navigator.mediaDevices.getUserMedia(constraints);
+            return stream;
+        } catch (err) {
+            throw err;
+        }
     }
 }
