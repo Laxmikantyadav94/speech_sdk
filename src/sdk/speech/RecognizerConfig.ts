4730452a2524e917ab0d5040d3aee8479275623f
Laxmikant Munshilal <laxmikantm@pune.smartek21.st21>
2019-12-19 13:23:21 +0530

    cleanup

--- src/sdk/speech/RecognizerConfig.ts
+++ src/sdk/speech/RecognizerConfig.ts
@@ -1,6 +1,6 @@
-import * as RecordRTC from "recordrtc";
+// import * as RecordRTC from "recordrtc";
 import * as io from "socket.io-client";
-import * as ss from "socket.io-stream";
+// import * as ss from "socket.io-stream";
 
 export class RecognizerConfig {
     protected socket: any;
@@ -9,60 +9,53 @@ export class RecognizerConfig {
 
     public async ioConnect(): Promise<any> {
         return new Promise(async (resolve: (value?: {} | PromiseLike<{}> | undefined) => void, reject: (reason?: any) => void) => {
-            try{
-                this.socket = io.connect("http://192.168.50.86:3000");
-                this.stream = await this.getMedia();
-                this.recorder = new RecordRTC.StereoAudioRecorder(this.stream, {
-                        mimeType: "audio/wav",
-                        type: "audio",
-                });
-                resolve();
-            }catch(err){
-                reject(err)
-            }
-        })
-       
-    }
-
-    public recordAudio(): void {
-        this.recorder.record();
-        console.log("recordnign started")
-    }
-
-    public stopAudio(): Promise<any> {
-        return new Promise((resolve: (value?: {} | PromiseLike<{}> | undefined) => void, reject: (reason?: any) => void) => {
             try {
-              this.recorder.stop((blob: any) => {
-                console.log("blob size....",blob);
-
-                this.stream = ss.createStream();
-                ss(this.socket).emit("audio", this.stream);
-                ss.createBlobReadStream(blob).pipe(this.stream);
-                console.log("testing...")
-                ss(this.socket).on("sttresult", (stream:any ,data: any) => {
-                    console.log("stt rerult stream....",stream);
-                    console.log("stt rerult....",data);
-                  if (data.err) {
-                    reject("Issue at DeepSpeech side");
-                  } else {
-                    resolve(data.text);
-                  }
-                });
-              });
+                this.socket = io.connect("http://192.168.50.86:3000");
+                // this.stream = await this.getMedia();
+                // this.recorder = new RecordRTC.StereoAudioRecorder(this.stream, {
+                //         mimeType: "audio/wav",
+                //         type: "audio",
+                // });
+                resolve();
             } catch (err) {
-              reject(err);
+                reject(err);
             }
-          });
+        });
     }
 
-    private async getMedia(): Promise<any> {
-        let stream = null;
-        const constraints = { audio: true };
-        try {
-            stream = await navigator.mediaDevices.getUserMedia(constraints);
-            return stream;
-        } catch (err) {
-            throw err;
-        }
-    }
+    // public recordAudio(): void {
+    //     this.recorder.record();
+    // }
+
+    // public stopAudio(): Promise<any> {
+    //     return new Promise((resolve: (value?: {} | PromiseLike<{}> | undefined) => void, reject: (reason?: any) => void) => {
+    //         try {
+    //           this.recorder.stop((blob: any) => {
+    //             this.stream = ss.createStream();
+    //             ss(this.socket).emit("audio", this.stream);
+    //             ss.createBlobReadStream(blob).pipe(this.stream);
+    //             ss(this.socket).on("sttresult", (stream: any , data: any) => {
+    //               if (!stream || data.err) {
+    //                 reject("Issue at DeepSpeech side");
+    //               } else {
+    //                 resolve(data.text);
+    //               }
+    //             });
+    //           });
+    //         } catch (err) {
+    //           reject(err);
+    //         }
+    //       });
+    // }
+
+    // private async getMedia(): Promise<any> {
+    //     let stream = null;
+    //     const constraints = { audio: true };
+    //     try {
+    //         stream = await navigator.mediaDevices.getUserMedia(constraints);
+    //         return stream;
+    //     } catch (err) {
+    //         throw err;
+    //     }
+    // }
 }
